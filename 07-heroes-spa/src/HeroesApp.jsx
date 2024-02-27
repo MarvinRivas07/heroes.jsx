408e37a29590fc4fb97d6b4140375eb66ec85aa5
Marvin <88003076+MarvinRivas07@users.noreply.github.com>
2024-02-27 10:28:28 -0600

    ...

--- 07-heroes-spa/src/HeroesApp.jsx
+++ 07-heroes-spa/src/HeroesApp.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import AppRouter from './router/AppRouter'
+import { AuthProvider } from './auth'
 
 const HeroesApp = () => {
   return (
-    <>
+
     
+    <AuthProvider>
+
     <AppRouter/>
 
-    </>
+    </AuthProvider>
+
   )
 }
 
--- 07-heroes-spa/src/auth/context/authReducer.js
+++ 07-heroes-spa/src/auth/context/authReducer.js
@@ -1,19 +1,21 @@
 import { types } from "../types/types";
 
 
-const initialState = {
-    logged: false,
-}
-
-
-export const  authReducer = (state, action) => {
+export const  authReducer = (state = {}, action) => {
 
     switch (action.type) {
         case types.login:
-            return state;
+            return {
+                ...state,
+                logged: true,
+                user: action.payload
+            }
 
             case types.logout:
-                return state;
+                return {
+                logged: false,
+               
+                }
     
         default:
             return state;
